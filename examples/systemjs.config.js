1cd0954059b4082fce4291f5b1d74e16c5889567
Ruslan Arkhipau <dethariel@gmail.com>
2016-08-31 14:29:43 -0700

    Changes spacing to be 2 spaces

--- examples/systemjs.config.js
+++ examples/systemjs.config.js
@@ -1,54 +1,54 @@
-(function(global) {
-  global.initSystemJS = function(recaptchaMapping, loadForms) {
+(function (global) {
+  global.initSystemJS = function (recaptchaMapping, loadForms) {
     // lock in the angular package version; do not let it float to current!
     var ngVer = '@2.0.0-rc.5';
     var formsVersion = '@0.3.0';
 
     //map tells the System loader where to look for things
-    var  map = {
-        'examples':       './examples/app',
+    var map = {
+      'examples': './examples/app',
 
-        '@angular':      'https://npmcdn.com/@angular', // sufficient if we didn't pin the version
-        'rxjs':          'https://npmcdn.com/rxjs@5.0.0-beta.6',
-        'ts':            'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
-        'typescript':    'https://npmcdn.com/typescript@1.8.10/lib/typescript.js',
-        'ng2-recaptcha': recaptchaMapping,
+      '@angular': 'https://npmcdn.com/@angular', // sufficient if we didn't pin the version
+      'rxjs': 'https://npmcdn.com/rxjs@5.0.0-beta.6',
+      'ts': 'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
+      'typescript': 'https://npmcdn.com/typescript@1.8.10/lib/typescript.js',
+      'ng2-recaptcha': recaptchaMapping,
     };
 
     //packages tells the System loader how to load when no filename and/or no extension
     var packages = {
-        'examples':       { defaultExtension: 'ts' },
-        'ng2-recaptcha':  { main: 'ng2-recaptcha.js', defaultExtension: 'js' },
+      'examples': { defaultExtension: 'ts' },
+      'ng2-recaptcha': { main: 'ng2-recaptcha.js', defaultExtension: 'js' },
     };
 
     if (loadForms) {
-        map['@angular/forms'] = 'https://npmcdn.com/@angular/forms' + formsVersion;
-        packages['@angular/forms'] =  { main: 'bundles/forms.umd.js', defaultExtension: 'js' };
+      map['@angular/forms'] = 'https://npmcdn.com/@angular/forms' + formsVersion;
+      packages['@angular/forms'] = { main: 'bundles/forms.umd.js', defaultExtension: 'js' };
     }
 
     var ngPackageNames = [
-        'common',
-        'compiler',
-        'core',
-        'platform-browser',
-        'platform-browser-dynamic',
+      'common',
+      'compiler',
+      'core',
+      'platform-browser',
+      'platform-browser-dynamic',
     ];
 
     // Add map entries for each angular package
     // only because we're pinning the version with `ngVer`.
-    ngPackageNames.forEach(function(pkgName) {
-        map['@angular/'+pkgName] = 'https://npmcdn.com/@angular/' + pkgName + ngVer;
+    ngPackageNames.forEach(function (pkgName) {
+      map['@angular/' + pkgName] = 'https://npmcdn.com/@angular/' + pkgName + ngVer;
     });
 
     // Add package entries for angular packages
-    ngPackageNames.forEach(function(pkgName) {
-        packages['@angular/'+pkgName] = { main: 'bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+    ngPackageNames.forEach(function (pkgName) {
+      packages['@angular/' + pkgName] = { main: 'bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
     });
 
     var config = {
-        // DEMO ONLY! REAL CODE SHOULD NOT TRANSPILE IN THE BROWSER
-        transpiler: 'ts',
-        typescriptOptions: {
+      // DEMO ONLY! REAL CODE SHOULD NOT TRANSPILE IN THE BROWSER
+      transpiler: 'ts',
+      typescriptOptions: {
         "target": "es5",
         "module": "system",
         "moduleResolution": "node",
@@ -60,14 +60,14 @@
         "noImplicitAny": true,
         "listFiles": false,
         "noLib": false
-        },
-        meta: {
+      },
+      meta: {
         'typescript': {
-            "exports": "ts"
+          "exports": "ts"
         }
-        },
-        map: map,
-        packages: packages
+      },
+      map: map,
+      packages: packages
     }
 
     System.config(config);
